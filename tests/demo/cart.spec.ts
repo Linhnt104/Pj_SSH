46d7c636332fc54db45d265c96599735e0790e65
linhnt <nguyenthilinh2150@gmail.com>
2025-11-30 21:22:31 +0700

    add pj_ssh/cart

--- tests/demo/cart.spec.ts
+++ tests/demo/cart.spec.ts
@@ -1,7 +1,7 @@
 import {test,expect} from '@playwright/test';
-import FilterProductPage from '../../pages/FilterProductPage';
-import HomePage from '../../pages/HomePage';
-import LoginPage from '../../pages/LoginPage';
+import FilterProductPage from '../../pages/AutomationExercise/FilterProductPage';
+import LoginPage from '../../pages/AutomationExercise/LoginPage';
+import HomePage from '../../pages/AutomationExercise/HomePage';
 
 test('add product to cart successfully',async ({page}) => {
     const homePage = new HomePage(page);
@@ -12,11 +12,11 @@ test('add product to cart successfully',async ({page}) => {
     await homePage.clickSignUpOrLoginButton();
     await loginPage.loginWithValidAccount("admin234@gmail.com","123456");
     await filterProductPage.scrollPage();
-    await filterProductPage.viewDetailProduct();
+    // await filterProductPage.viewDetailProduct();
     await filterProductPage.verifyAddedDialog();
     await filterProductPage.viewCart();
     // await filterProductPage.verifyDataCart();
 
     await page.waitForTimeout(5000);
-    
+
 })
